162b60f1ce649eace0b2dc410735a772e36b0659
许晓东 <763795151@qq.com>
2020-03-30 23:05:53 +0800

    发送邮件备份数据，单元测试集成

---
+++ src/main/java/com/xuxd/baishun/common/SendMailClient.java
@@ -0,0 +1,9 @@
+package com.xuxd.baishun.common;
+
+/**
+ * @Auther: 许晓东
+ * @Date: 20-3-30 21:47
+ * @Description:
+ */
+public class SendMailClient {
+}
---
+++ src/test/java/com/xuxd/baishun/common/SendMailClientTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * @Auther: 许晓东
+ * @Date: 20-3-30 22:03
+ * @Description:
+ */
+public class SendMailClientTest {
+
+}
