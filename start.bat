3946ecf9df024256dd7f75a62c94a5ac129ccaac
许晓东 <xu.xiaodong@asiainfo-sec.com>
2021-03-07 09:46:44 +0800

    windows启动脚本修改

--- start.bat
+++ start.bat
@@ -6,13 +6,11 @@ rem -Dbackup.enable=true target/baishun.jar
 
 echo %cd%
 set MAIN_CLASS=org.springframework.boot.loader.JarLauncher
-set JAVA_HOME=F:\jdk\jdk_jre\jre1.8.0_66
+set JAVA_HOME=jre1.8.0_66
 set JAVA_CMD=%JAVA_HOME%\bin\java
 set JAVA_OPTS=-Dmail.username=xxx@163.com ^
 				-Dmail.password=email_password    ^
 				-Dmail.from=xxx@163.com ^
-				-Dmail.to=yyy@163.com   ^
-				-Dbackup.enable=true
-rem %JAVA_CMD% -cp .;target\baishun.jar;%JAVA_HOME%\ %JAVA_OPTS% %MAIN_CLASS%
-%JAVA_CMD% -cp .;G:\baishun\BaiShun\target\baishun.jar;%JAVA_HOME%\ %JAVA_OPTS% %MAIN_CLASS%
-rem pause
+				-Dmail.to=to_xxx@qq.com   ^
+				-Dbackup.enable=false
+%JAVA_CMD% -cp .;baishun.jar;%JAVA_HOME%\ %JAVA_OPTS% %MAIN_CLASS% --server.address=127.0.0.1
